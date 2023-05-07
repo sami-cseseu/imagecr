2c689aa82347c8f93279a451a43a6cafcfec7392
Samiuzzaman Khan <>
2023-05-07 17:06:19 +0200

    Add initial route and pages

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
-<html>
-    <body>
-        <h3>Hello World</h3>
-    </body>
-</html>
+<?php
+$request = $_SERVER['REQUEST_URI'];
+
+switch ($request) {
+    case '':
+    case '/':
+        require __DIR__.'/app/index.php';
+        break;
+
+
+    default:
+        http_response_code(404);
+        require __DIR__.'/app/404.php';
+        break;
+}
