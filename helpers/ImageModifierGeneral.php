0b1a892b72dc9ba3bdd32a071dea069aef3f3ba6
Samiuzzaman Khan <>
2023-05-07 17:20:28 +0200

    Create type image for further manipulation from image object

--- helpers/ImageModifierGeneral.php
+++ helpers/ImageModifierGeneral.php
@@ -1,9 +1,30 @@
 <?php
 
 require_once 'ImageModifierInterface.php';
+require_once 'ImageInterface.php';
 
 class ImageModifierGeneral implements ImageModifierInterface
 {
+    /**
+     * @var false|GdImage|resource
+     */
+    protected $formattedImage;
+
+    /**
+     * @param  ImageInterface  $originalImage
+     * @throws Exception
+     */
+    public function __construct(protected ImageInterface $originalImage)
+    {
+        $this->formattedImage = match ($originalImage->type()) {
+            'image/jpeg' => imagecreatefromjpeg($originalImage->path()),
+            'image/gif' => imagecreatefromgif($originalImage->path()),
+            'image/png' => imagecreatefrompng($originalImage->path()),
+            'image/webp' => imagecreatefromwebp($originalImage->path()),
+            'image/bmp' => imagecreatefrombmp($originalImage->path()),
+            default => throw new Exception('Unsupported Format.'),
+        };
+    }
 
     public function resize(int $width, int $height): void
     {
