dc32241223e5c6fc59d202b2f99655db6a161d7f
Samiuzzaman Khan <>
2023-05-07 18:00:10 +0200

    Show all available images from storage in default route

--- app/index.php
+++ app/index.php
@@ -1,6 +1,23 @@
 <html>
-		<body>
-				<h3>Hello World</h3>
-		</body>
-</html>
+<body>
+		<h3>Available Images</h3>
+		<div>
+        <?php
+        $iterator = new FilesystemIterator(STORAGE_IMAGES);
+        foreach ($iterator as $file) {
+            $fileName = $file->getFilename();
+            $currentUrl = (empty($_SERVER['HTTPS']) ? 'http' : 'https')."://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+            echo '
+                <img src="'.$file->getPath().'/'.$fileName.'" style="max-width:200px">
+								<br/>
+								<p>Image Name : '.$fileName.'</p>
+								<p>Demo Resize Request : '.$currentUrl.'resize?fileName='.$fileName.'&width=100&height=100</p>
+								<p>Demo Crop Request : '.$currentUrl.'crop?fileName='.$fileName.'&width=100&height=100</p>
+						';
+        }
+        ?>
 
+		</div>
+
+</body>
+</html>
